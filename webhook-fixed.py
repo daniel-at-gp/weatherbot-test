e88417ba307f18e614639d829bf3e370056c0f79
Daniel <35252939+daniel-at-gp@users.noreply.github.com>
2018-09-04 16:16:01 +0700

    Update webhook-fixed.py

--- webhook-fixed.py
+++ webhook-fixed.py
@@ -36,7 +36,7 @@ def makeResponse(req):
             condition= weather[i]['weather'][0]['description']
             break
 
-    speech = "Return4 " + city + date +" i= " + i
+    speech = "Return4 " + city + date +" i= " + weather
     return {
     "speech": speech,
     "displayText": speech,

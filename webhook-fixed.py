70941e200b84281e8fc5964d55e592ed2ed37405
Daniel <35252939+daniel-at-gp@users.noreply.github.com>
2018-09-04 15:50:14 +0700

    Update webhook-fixed.py

--- webhook-fixed.py
+++ webhook-fixed.py
@@ -27,18 +27,20 @@ def makeResponse(req):
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
-    speech = "Query for"+city+ "for "+date+"X"
+    r=requests.get('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=9812c8a8af116387d12e7a699ad13a96')
+    json_object = r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition= weather[i]['weather'][0]['description']
+            break
+    speech = "The forecast for "+city+ " for "+date+" is "+condition
     return {
     "speech": speech,
     "displayText": speech,
     "source": "apiai-weather-webhook"
     }
-  
-    return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "apiai-weather-webhook"
-    }
+
 
                             
 

5a3acb3b031619c1983422da6dcd8ffd45e33aa5
Daniel <35252939+daniel-at-gp@users.noreply.github.com>
2018-09-04 17:11:02 +0700

    Update webhook-fixed.py

--- webhook-fixed.py
+++ webhook-fixed.py
@@ -33,10 +33,11 @@ def makeResponse(req):
     weather=json_object['list']
     check ="CHK: " 
     for i in range(0,30):
-        check = check + " DT:" + weather[i]['dt_txt'] + " W:" + weather[i]['weather'][0]['description']
         weatherDate = weather[i]['dt_txt']
-        if date[10:] = weatherDate[10:]:
+        check = check + " DT:" + weatherDate + "*" + weatherDate[10:] + " W:" + weather[i]['weather'][0]['description']
+#        if date[10:] = weatherDate[10:]:
 #            check = check + " " + weather[i]['dt_text']  +" "
+        if date in weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
             break
 

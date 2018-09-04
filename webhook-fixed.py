eeef49a9b07c33653e5ba9c41550f3f48b24db8a
Daniel <35252939+daniel-at-gp@users.noreply.github.com>
2018-09-04 15:15:45 +0700

    Update webhook-fixed.py

--- webhook-fixed.py
+++ webhook-fixed.py
@@ -23,33 +23,26 @@ def webhook():
     return r
 
 def makeResponse(req):
-    speech = print(json.dumps(req, indent=4))
-    return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "apiai-weather-webhook"
-    }
-
-    if req.get("result").get("action") != "fetchWeatherForecast":
-        speech = "req"
-        return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "apiai-weather-webhook"
-        }
-#        return {}
     result = req.get("result")
-    parameters = result.get("parameters")
+    parameters = result.get(parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
- 
-    speech = "damned if I don't"
+                            
+    r=requests.get('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=9812c8a8af116387d12e7a699ad13a96')
+    json_object = r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition= weather[i]['weather'][0]['description']
+            break
+    speech = "The forecast for"+city+ "for "+date+" is "+condition
     return {
     "speech": speech,
     "displayText": speech,
     "source": "apiai-weather-webhook"
     }
 
+                            
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
